86ed7c4185386b457f4b3d8eaf86ab4d6a1d2ce0
sammychvz <sammy_chvz17@outlook.com>
2022-03-27 22:11:08 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Nav from './componets/Nav';
 import About from './componets/About';
 
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
